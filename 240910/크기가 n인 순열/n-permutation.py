de83f2c9d74e0a84007305e12fa0b4d69649bddb
kukuhaza <57826222+kukuhaza@users.noreply.github.com>
2024-09-10 14:20:27 +0900

    Title: 크기가 n인 순열, Time: 249ms, Memory: 29MB, Status: Passed - CodeTree

--- 240910/크기가 n인 순열/n-permutation.py
+++ 240910/크기가 n인 순열/n-permutation.py
@@ -1,16 +1,19 @@
 n = int(input())
-used = [False] * (n+1)
+used = [False] * n
 path = []
+array = []
+for i in range(1,n+1):
+    array.append(i)
 
 def backtracking():
-    if len(path) == n:
+    if len(path) == len(array):
         print(*path)
 
-    for i in range(1, n+1):
+    for i in range(n):
         if used[i]:
             continue
         used[i] = True
-        path.append(i)
+        path.append(array[i])
 
         backtracking()
 
